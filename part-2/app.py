4de61fdb85d861d22223b5c531484ca791264cc0
Nikita Ahirrao <nikitaahirrao@gmail.com>
2026-01-07 12:59:06 +0530

    Part 2: Templates - Rendering HTML Files

--- part-2/app.py
+++ part-2/app.py
@@ -19,6 +19,11 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/internship')
+def internship_task():
+    return render_template('internship.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
